dd64e01984acbe288d5deaff4fa944ee66c3eec2
Kamyar <88121159+kmyrnx@users.noreply.github.com>
2022-05-18 01:30:05 +0430

    Update winston logs directory creation strategy

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const path = require('path');
+const { mkdirSync, existsSync } = require('fs');
 const app = require('./app');
 const { connectToDatabase } = require('./db/conn');
+
+// Create the log directory if it doesn't exist
+if (!existsSync(path.join(__dirname, './logs/app'))) {
+  mkdirSync(path.join(__dirname, './logs/app'), { recursive: true });
+}
 
 require('dotenv').config();
 
